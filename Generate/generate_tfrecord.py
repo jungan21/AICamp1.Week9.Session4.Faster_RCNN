e4efacdb2dcca512ff661657435385a00e66f917
Jun Gan <jun.gan@scotiabank.com>
2019-01-20 22:17:09 -0500

    add comments

--- Generate/generate_tfrecord.py
+++ Generate/generate_tfrecord.py
@@ -1,3 +1,6 @@
+
+# 把 wider_face_train_bbx_gt.txt 里这种格式，转化成tensforlow能识别的tf record 格式
+
 import io
 from PIL import Image
 import os
@@ -35,11 +38,14 @@ def create_tf_example(image_info, path):
     # 读出图片
     with tf.gfile.FastGFile(os.path.join(path, '{}'.format(image_info['filename'].rstrip())), 'rb') as fid:
         encoded_jpg = fid.read()
+
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
+
     # 得到图片的长宽
     width, height = image.size
     filename = image_info['filename'].encode('utf8')
+    #???
     image_format = b'jpg'
 
     xmins = []
@@ -54,9 +60,13 @@ def create_tf_example(image_info, path):
         xmaxs.append(xmax / width)
         ymins.append(ymin / height)
         ymaxs.append(ymax / height)
-        classes_text.append(b'face')
-        classes.append(1)
+        classes_text.append(b'face')  #类别只有两类，是人脸，不是人脸
+        classes.append(1) # 用1表示，是人脸
 
+
+    # https://www.tensorflow.org/api_docs/python/tf/train/Example i.e. A ProtocolMessage
+    # https://www.tensorflow.org/tutorials/load_data/tf-records#creating_a_tfexample_message  
+    # 因为项目用的是tensorflow train好的model, 所以下面的名字都是固定了 都是约定好的 e.g. image/height
     tf_example = tf.train.Example(features=tf.train.Features(feature={
         'image/height': int64_feature(height),
         'image/width': int64_feature(width),
@@ -73,12 +83,20 @@ def create_tf_example(image_info, path):
     }))
     return tf_example
 
+# 读入 wider_face_train_bbx_gt.txt ， gt 格式的数据
 def main(_):
     # 指定TFRecord文件输出路径
     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
 
     # 图片所在的绝对路径
     path = os.path.join(os.getcwd(), FLAGS.images_path)
+
+
+    # sample gt 文件内容
+    #	0--Parade/0_Parade_marchingband_1_465.jpg
+	#	126
+	#	345 211 4 4 2 0 0 0 2 0 
+	# 	331 126 3 3 0 0 0 1 0 0 
     
     # 打开GT的文件
     with open(FLAGS.gt_input, 'r') as f:
@@ -97,11 +115,14 @@ def main(_):
                 if inputs[0].rstrip().isdigit():
                     continue
 
-                # 如果是文件名，且不表示第一张图片，把上一张图片的信息转换为tf.example.
+                # 如果是文件名，且不表示第一张图片，就表示上一张图片的数据读完了，就把上一张图片的信息转换为tf.example.
                 if not is_first_image:
                     tf_example = create_tf_example(image_info, path)
                     writer.write(tf_example.SerializeToString())
 
+                    # 没读完一个文件的数据，就把这个类似于buffer功能的image_info变量置空
+                    # 这里之所以需要 xmin, xmax, 是因为tensorflow API 这么定义的： 因为tensorflow实现的api需要用xmin xmax ymin ymax的形式
+                    # https://github.com/jungan21/models/blob/master/research/object_detection/g3doc/using_your_own_dataset.md
                     image_info = {'filename' : '', 'xmin': [], 'xmax': [], 'ymin': [], 'ymax': []}
 
                 # 更新 image_info 里面的 filename.
@@ -111,7 +132,9 @@ def main(_):
             # 这一行是10个不同的数字
             else:
                 x1, y1, w, h, blur, expression, illumination, invalid, occlusion, pose, _= inputs
+                # 如果不转换成float, 就是整数， 在做除法的时候，就会变成0
                 image_info['xmin'].append(float(x1))
+                # ??? 为什么要存下xmax, 而不是你直接存下width i.e. w
                 image_info['xmax'].append(float(x1) + float(w))
                 image_info['ymin'].append(float(y1))
                 image_info['ymax'].append(float(y1) + float(h))
--- README.md
+++ README.md
@@ -10,6 +10,20 @@ Correct data: zhouji1994/datasets/correct-widerface-data/1
 
 TF Object Detection code: martinhoest/datasets/tf_models/1
 
+因为我们这里model用的参数，都是来自于其他已经train好的model里的参数，下面的文件都是包含已经train好的其他model里的参数
+
+zhouji1994/datasets/widerface/1/checkpoint
+
+zhouji1994/datasets/widerface/1/.DS_Store
+
+之前train 好的model的参数都在这里：zhouji1994/datasets/widerface/1/model.ckpt.data-00000-of-00001
+
+zhouji1994/datasets/widerface/1/pipeline.config
+
+zhouji1994/datasets/widerface/1/model.ckpt.meta
+
+zhouji1994/datasets/widerface/1/frozen_inference_graph.pb
+
 
 ## Run command
 
@@ -22,7 +36,7 @@ floyd login
 ```
 cd TrainingFiles_1
 
-floyd init '...'
+floyd init '...'  # <project name>
 
 floyd run --gpu --env tensorflow-1.5 --data zhouji1994/datasets/widerface/1:/training --data martinhoest/datasets/tf_models/1:/tf_models --tensorboard 'bash run.sh'
 ```
@@ -34,7 +48,7 @@ floyd run --gpu --env tensorflow-1.5 --data zhouji1994/datasets/widerface/1:/tra
 ```
 cd TrainingFiles_2
 
-floyd init '...'
+floyd init '...' # <project name>
 
 floyd run --gpu --env tensorflow-1.5 --data zhouji1994/datasets/widerface/1:/training --data martinhoest/datasets/tf_models/1:/tf_models --data zhouji1994/datasets/correct-widerface-data/1:/data --tensorboard 'bash run.sh'
 ```
